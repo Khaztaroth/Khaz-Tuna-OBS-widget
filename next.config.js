2a71ce84bc072b4ef3c8c69f641d1cf8c4b1ffb0
khaztaroth <astaroth503@gmail.com>
2023-09-09 23:26:22 -0300

    inital commit things work in dev have to make it work as static export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['localhost']
+    },
+    output: 'export',
+}
 
 module.exports = nextConfig
